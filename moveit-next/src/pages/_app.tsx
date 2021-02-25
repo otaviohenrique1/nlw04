bdfc770b61fb5048b10f1696bde892a3f7318cf2
Otavio Henrique <OtavioHenLimSil@gmail.com>
2021-02-24 22:53:54 -0300

    Commit 7 dos arquivos

--- moveit-next/src/pages/_app.tsx
+++ moveit-next/src/pages/_app.tsx
@@ -1,12 +1,13 @@
+import { ChallengesProvider } from "../contexts/ChallengesContext";
 import "../styles/global.css";
 
 // _app -> carregado em todas as paginas
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <ChallengesProvider>
       <Component {...pageProps} />
-    </>
+    </ChallengesProvider>
   );
 }
 
